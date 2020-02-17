5025d718943f30d8fc85fabf31da1248bb70f826
Vlad Polevoi <vlad.polewoi1@gmail.com>
2020-02-17 13:32:39 -0800

    1. react basics 2634

---
+++ basics-react/src/Todo/TodoItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function TodoItem({ todo, index }) {
+  return (
+  <li>
+    <strong>{index + 1}.</strong>
+    {todo.title}
+  </li>
+  )
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number
+}
+
+export default TodoItem
+ 
