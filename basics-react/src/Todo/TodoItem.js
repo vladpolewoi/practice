ee35ec4394fae58b575672b85ef1e7539e748815
vlad polewoi <vlad.polewoi1@gmail.com>
2020-02-18 10:27:06 +0200

    part 2

--- basics-react/src/Todo/TodoItem.js
+++ basics-react/src/Todo/TodoItem.js
@@ -1,18 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function TodoItem({ todo, index }) {
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '.5rem 1rem',
+    border: '1px solid #cccccc',
+    borderRadius: '4px',
+    marginBottom: '.5rem',
+  },
+  input: {
+    marginRight: '1rem',
+  }
+}
+function TodoItem({ todo, index, onChange }) {
+  const classes = []
+
+  if (todo.completed) {
+    classes.push('done')
+  }
+
   return (
-  <li>
-    <strong>{index + 1}.</strong>
-    {todo.title}
+  <li style={styles.li}>
+    <span className={classes.join(' ')}>
+      <input
+        type="checkbox"
+        style={styles.input}
+        onChange={() => onChange(todo.id)}
+        checked={todo.completed}
+      />
+      <strong>{index + 1}.</strong>
+      &nbsp;
+      {todo.title}
+    </span>
+
+    <button className="rm">&times;</button>
   </li>
   )
 };
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
-  index: PropTypes.number
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem
