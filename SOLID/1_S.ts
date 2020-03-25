b2859f92f73e49caffb166862766c077b1c64eda
Vlad Polevoi <vlad.polewoi1@gmail.com>
2020-03-25 15:26:20 +0200

    SOLID - 1_S

---
+++ SOLID/1_S.ts
@@ -0,0 +1,44 @@
+// Single Responsibility Principal
+
+class Plane {
+  maxSpeed: Number
+  isFlying: Boolean
+
+  constructor(readonly model: string, maxSpeed: Number) {
+    this.maxSpeed = maxSpeed
+  }
+
+  fly(): void {
+    this.isFlying = true;
+  }
+
+  // 1. Class Plane have nothing to do with displaying his info in JSON
+  // so it's better to move it in new class
+  // class should be responsible only for itself
+
+  // getInfoInJSON(): PlaneInfo {
+  //   return {
+  //     model: this.model,
+  //     maxSpeed: this.maxSpeed,
+  //   }
+  // }
+}
+
+class InfoBox {
+  constructor(readonly plane: Plane) {}
+
+  // 2. Moved method to new class which is responsible
+  // only for displaying info
+
+  getInfoInJSON(): string {
+    return JSON.stringify({
+      model: this.plane.model,
+      maxSpeed: this.plane.maxSpeed,
+    });
+  }
+}
+
+const plane = new Plane('Airbus-37', 3453);
+const infoBox = new InfoBox(plane);
+
+console.log(infoBox.getInfoInJSON());
